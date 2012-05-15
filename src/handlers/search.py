b9d68b9c99f80cd5f45a0af7c2d94739df60b33a
kai cui <unasogno@hotmail.com>
2012-05-16 00:11:56 +0800

    search

--- src/handlers/search.py
+++ src/handlers/search.py
@@ -9,12 +9,31 @@ import config
 def get_search(url, headers, body):
   query = headers.get('QUERY')
   params = dict((n,v) for n, v in (i.split('=', 1) for i in query.split('&')))
-  text = params['text']
+  text = params['terms']
   search_query = helpers.decode_urlencoding(text)
   # helpers.log_search_query(search_query)
 
   terms = seg_txt(search_query)
-  print terms
+
+  database = xapian.Database('../indexes/')
+  enquire = xapian.Enquire(database)
+
+  l = []
+  for term in terms:
+    l.append(term)
+
+  q = xapian.Query(xapian.Query.OP_OR, l)
+
+  enquire.set_query(q)
+  matches = enquire.get_mset(0, 10)
+
+  print '%i results found.' % matches.get_matches_estimated()
+  print 'Result - %i:' % matches.size()
+
+  for m in matches:
+    print '%i: %i%% docid=%i [%s]' % (m.rank + 1, m.percent, m.docid,\
+    m.document.get_data())
+
   return 200, 'OK', ''
 
 logger = helpers.init_logger('search', config.LOG_PATH)
