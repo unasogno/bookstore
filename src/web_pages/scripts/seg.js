48eef5323506c0c83c6d68041270229de1af4c79
kai cui <unasogno@hotmail.com>
2012-05-19 00:23:25 +0800

    add ajax

--- src/web_pages/scripts/seg.js
+++ src/web_pages/scripts/seg.js
@@ -1,6 +1,19 @@
 
 $(document).ready(function(){
   $("#submit").click(function(){
-    alert($("#text").val());
+    var query = $("input#query").val();
+    if (0 != query.length) {
+      $.ajax({
+        url: "/search?query=".concat(query),
+        success: function(data, status, jqXHR) {
+          alert(data);
+        },
+        error: function(jqXHR, status, errorThrown) {
+          alert(status);
+        },
+        complete: function(jqXHR, status) {
+          alert('complete');
+        }
+    }
   });
 });
