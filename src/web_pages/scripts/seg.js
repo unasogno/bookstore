e3ae1aecd8089272abf858427ce8bfd2f238555e
kai cui <unasogno@hotmail.com>
2012-05-19 01:13:00 +0800

    show query url

--- src/web_pages/scripts/seg.js
+++ src/web_pages/scripts/seg.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
   $("#submit").click(function(){
     var query = $("input#query").val();
     if (0 != query.length) {
+      var ajax_url = "/search?query=".concat(query);
+      alert(ajax_url);
       $.ajax({
         url: "/search?query=".concat(query),
         success: function(data, status, jqXHR) {
