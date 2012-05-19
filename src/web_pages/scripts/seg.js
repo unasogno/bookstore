a0d51e5c60e28d49f850796583ac2fab72444914
kai cui <unasogno@hotmail.com>
2012-05-19 19:34:21 +0800

    onComplete

--- src/web_pages/scripts/seg.js
+++ src/web_pages/scripts/seg.js
@@ -1,4 +1,22 @@
 var search_result;
+
+function submitSearch(query) {
+  var query = $("input#query").val();
+  query = encodeURI(query);
+  if (0 != query.length) {
+    var ajax_url = "/search?query=".concat(query);
+    console.log(ajax_url);
+    $.ajax({
+      url: "/search?query=".concat(query),
+      success: onSuccess,
+      error: onError,
+      complete: onComplete
+    });
+  } else {
+    alert('请输入搜索内容');
+  }
+}
+
 function onSuccess(data, status, jqXHR) {
   alert(status);
   search_result = data;
@@ -10,8 +28,11 @@ function onError(jqXHR, status, errorThrown) {
 }
 
 function onComplete(jqXHR, status) {
-  alert(status);
-  alert(search_result);
+  if (null == search_result) return;
+  if (undefined == search_result) return;
+  idList = jQuery.parseJSON(search_result);
+  if (0 == idList.length) return;
+  alert(idList[0]);
 }
 
 $(document).ready(function(){
