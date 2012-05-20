f0bee4f2c3ae4aebdc40055fefb516af631e81ef
kai cui <unasogno@hotmail.com>
2012-05-21 00:12:58 +0800

    modify books.py

--- src/handlers/books.py
+++ src/handlers/books.py
@@ -6,14 +6,23 @@ import data_access as da
 import config
 import helpers
 
+def __extract_arguments(s):
+  if (None == s):
+    return None;
+  return dict((n,v) for n, v in (i.split('=', 1) for i in s.split('&')))
+
 def post(path, query, body):
   if None == body:
     return 400, 'Bad Request', 'book id expected', None
   
   try:  
-    id_list = helpers.decode_urlencoding(body)
-    logger.debug(id_list)
-    if '' == id_list.strip():
+    decoded = helpers.decode_urlencoding(body)
+    arguments = __extract_arguments(decoded)
+    if 'idList' not in arguments:
+      return 400, 'Bad Request', 'book id expected', None
+
+    id_str = arguments['idList']
+    if '' == id_str.strip():
       return 400, 'Bad Request', 'book id expected', None
     '''  
     if (not book_id.isdigit()):
@@ -23,10 +32,8 @@ def post(path, query, body):
     return 500, 'Internal Server Error', 'unknown error', None
   
   try:
-    ''' 
-    return 200, 'OK', da.query_books(id_list), {
+    return 200, 'OK', da.query_books(id_str), {
       'Content-Type': 'application/json;charset=UTF-8'}
-    '''
     return 200, 'OK', 'Test', None 
   except:
     return 500, 'Internal Server Error' 'database error', None
