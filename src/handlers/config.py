1a26e47531d0ff3feeffad00f62999e4f05614b5
kai cui <unasogno@hotmail.com>
2012-06-03 13:47:16 +0800

    add rsa testing module

--- src/handlers/config.py
+++ src/handlers/config.py
@@ -10,9 +10,9 @@ class HandlerConfig(object):
 LOG_PATH = 'log/search.log'
 
 HANDLER_CONFIG = {
+    'books':  HandlerConfig('books',  'tcp://127.0.0.1:9997', 'tcp://127.0.0.1:9996'),
+    'book':   HandlerConfig('book',   'tcp://127.0.0.1:9995', 'tcp://127.0.0.1:9994'),
     'search': HandlerConfig('search', 'tcp://127.0.0.1:9993', 'tcp://127.0.0.1:9992'),
-    'book': HandlerConfig('book', 'tcp://127.0.0.1:9995', 'tcp://127.0.0.1:9994'),
-    'books': HandlerConfig('books', 'tcp://127.0.0.1:9997', 'tcp://127.0.0.1:9996'),
-    'signup': HandlerConfig('signup', 'tcp//127.0.0.1:9989', 'tcp://127.0.0.1.9988')
-    'signin': HandlerConfig('signin', 'tcp//127.0.0.1:9987', 'tcp://127.0.0.1.9986')
+    'signup': HandlerConfig('signup', 'tcp://127.0.0.1:9989', 'tcp://127.0.0.1.9988'),
+    'signin': HandlerConfig('signin', 'tcp://127.0.0.1:9987', 'tcp://127.0.0.1.9986')
   }
