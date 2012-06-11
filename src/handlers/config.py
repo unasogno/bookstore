f694498bdee858cef33ae5369d1764357f287762
kai cui <unasogno@hotmail.com>
2012-06-12 01:07:09 +0800

    signin.py

--- src/handlers/config.py
+++ src/handlers/config.py
@@ -7,12 +7,22 @@ class HandlerConfig(object):
     self.send_spec = send_spec
     self.recv_spec = recv_spec
 
+HANDLER_CONFIG = {
+    'books':  HandlerConfig(
+      'books',  'tcp://127.0.0.1:9997', 'tcp://127.0.0.1:9996'),
+    'book':   HandlerConfig(
+      'book',   'tcp://127.0.0.1:9995', 'tcp://127.0.0.1:9994'),
+    'search': HandlerConfig(
+      'search', 'tcp://127.0.0.1:9993', 'tcp://127.0.0.1:9992'),
+    'signup': HandlerConfig(
+      'signup', 'tcp://127.0.0.1:9989', 'tcp://127.0.0.1:9988'),
+    'signin': HandlerConfig(
+      'signin', 'tcp://127.0.0.1:9987', 'tcp://127.0.0.1:9986')
+  }
+
 LOG_PATH = 'log/search.log'
 
-HANDLER_CONFIG = {
-    'books':  HandlerConfig('books',  'tcp://127.0.0.1:9997', 'tcp://127.0.0.1:9996'),
-    'book':   HandlerConfig('book',   'tcp://127.0.0.1:9995', 'tcp://127.0.0.1:9994'),
-    'search': HandlerConfig('search', 'tcp://127.0.0.1:9993', 'tcp://127.0.0.1:9992'),
-    'signup': HandlerConfig('signup', 'tcp://127.0.0.1:9989', 'tcp://127.0.0.1:9988'),
-    'signin': HandlerConfig('signin', 'tcp://127.0.0.1:9987', 'tcp://127.0.0.1:9986')
-  }
+DB_HOST = '127.0.0.1'
+DB_USER = 'bookstore_app'
+DB_PASSWORD = '123'
+DB_INST = 'bookstore'
---
+++ src/handlers/signin.py
@@ -0,0 +1,79 @@
+# _*_ coding=utf8 _*_
+
+import rsa
+import hashlib
+import json
+import base64
+import handler
+import user
+import config
+import helpers
+
+def load_private():
+  with open('priv.txt', 'r') as fp:
+    pem = fp.read()
+    fp.close()
+  return rsa.PrivateKey.load_pkcs1(pem)
+
+def decrypt(cipher, priv):
+  global logger
+  cipher = helpers.decode_urlencoding(cipher)
+  cipher = int(cipher, 16)
+  logger.debug('cipher = %d', cipher)
+
+  cipher = rsa.transform.int2bytes(cipher)
+  return rsa.decrypt(cipher, priv)
+
+def create_with_email(identity, password):
+  pass
+
+def create_with_phone_number(identity, password):
+  pass
+
+def put(path, headers, body):
+  pass
+
+def get(path, headers, body):
+  global logger
+
+  query = headers.get('QUERY')
+  arguments = helpers.parse_query_string(query)
+
+  try:
+    priv = load_private()
+    identity_str = decrypt(arguments['identity'], priv)
+    identity = user.Identity.load(identity_str)
+
+    password = decrypt(arguments['password'], priv)
+    identity.validate(identity, password)
+
+    return 200, 'OK', 'validation passed.', {'Content-Type': 'text/plain'}
+  except ValueError:
+    logger.error(helpers.format_exception())
+    return 400, 'Bad Request', 'Missing argument(s).', {}
+  except rsa.DecryptionError:
+    logger.error(helpers.format_exception())
+    return 500, 'Internal Server Error', 'Decryption failed', {}
+  except user.IdentityError:
+    logger.error(helpers.format_exception())
+    return 400, 'Bad Request', 'Unrecognized identity.', {}
+
+def post(path, headers, body):
+  pass
+
+def delete(path, headers, body):
+  pass
+
+handlers = {
+  'PUT': put, 'GET': get, 'post': post, 'delete': delete }
+logger = helpers.init_logger('signup', config.LOG_PATH)
+
+if __name__ == '__main__':
+  
+  try:
+    handler_config = config.HANDLER_CONFIG['signup']
+    handler.run(handler_config.send_spec, handler_config.recv_spec, handlers)
+  except:
+    logger.error(helpers.format_exception())
+else:
+  handler.handlers_registry[__name__] = handlers
