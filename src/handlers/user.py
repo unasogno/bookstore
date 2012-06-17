d32217046a31f5b63ff8d905579c16dad43f8817
kai cui <unasogno@hotmail.com>
2012-06-17 14:37:06 +0800

    signin

--- src/handlers/user.py
+++ src/handlers/user.py
@@ -169,7 +169,7 @@ class Database(object):
       r.fetch_row()  
       return user_id
 
-    return self._exec(hanler)
+    return self._exec(handler)
 
   def email_exists(self, email):
     return self.identity_exists('email', email)
