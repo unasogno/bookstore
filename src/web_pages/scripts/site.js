05e7818353b45f1b8e3b75ea816d2ec4b32213e3
kai cui <unasogno@hotmail.com>
2012-06-22 22:24:43 +0800

    add login handlers

--- src/web_pages/scripts/site.js
+++ src/web_pages/scripts/site.js
@@ -4,8 +4,15 @@ function login(dialog, success, failure) {
     .dialog({
       height: 200,
       buttons: {
-        "登录": function() {},
-        "清除": function() {}
+        "登录": function() {
+          var identity = $("#login_identity").val();
+          var password = $("#login_password").val();
+          signin(identity, password);
+        },
+        "清除": function() {
+          $("#login_identity").val("");
+          $("#login_password").val("");
+        }
       },
       autoOpen: false,
       modal: true,
@@ -15,4 +22,28 @@ function login(dialog, success, failure) {
     });
 
   $(dialog).dialog("open");
+}
+
+function signin(identity, password) {
+  $.get("/rsa/public.key",
+        function(result) {
+          var pub = result;
+          var pubkey = new RSAKey();
+          pubkey.setPublic(pub, '10001');
+          identity = pubkey.encrypt(identity);
+          password = pubkey.encrypt(password);
+
+          $.ajax({
+            url:"/api/signin", 
+            data: {"identity": identity,
+                   "password": password
+            }, 
+            success: function(data) {
+              alert(data);
+            },
+            error: function(jqXHR, status, exception) {
+              alert(exception);
+            }
+          });
+        });
 }
