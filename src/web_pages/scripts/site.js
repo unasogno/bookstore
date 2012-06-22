7a82b9b78e2d985940095a43579cee8c28c5f0fb
kai cui <unasogno@hotmail.com>
2012-06-22 22:43:53 +0800

    add login handlers

--- src/web_pages/scripts/site.js
+++ src/web_pages/scripts/site.js
@@ -25,25 +25,24 @@ function login(dialog, success, failure) {
 }
 
 function signin(identity, password) {
-  $.get("/rsa/public.key",
-        function(result) {
-          var pub = result;
-          var pubkey = new RSAKey();
-          pubkey.setPublic(pub, '10001');
-          identity = pubkey.encrypt(identity);
-          password = pubkey.encrypt(password);
+  $.get("/rsa/public.key", function(result) {
+    var pub = result;
+    var pubkey = new RSAKey();
+    pubkey.setPublic(pub, '10001');
+    identity = pubkey.encrypt(identity);
+    password = pubkey.encrypt(password);
 
-          $.ajax({
-            url:"/api/signin", 
-            data: {"identity": identity,
-                   "password": password
-            }, 
-            success: function(data) {
-              alert(data);
-            },
-            error: function(jqXHR, status, exception) {
-              alert(exception);
-            }
-          });
-        });
+    $.ajax({
+      url:"/api/signin", 
+      data: {"identity": identity,
+             "password": password
+      }, 
+      success: function(data) {
+        alert(data);
+      },
+      error: function(jqXHR, status, exception) {
+        alert(exception);
+      }
+    });
+  });
 }
