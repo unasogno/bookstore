6d5a05e087a67c5aff3fb6607a892d9ece8625f0
kai cui <unasogno@hotmail.com>
2012-06-22 17:11:30 +0800

    add site.js

--- src/web_pages/scripts/site.js
+++ src/web_pages/scripts/site.js
@@ -1,6 +1,7 @@
 function login(dialog, success, failure) {
   $(dialog)
-    .html("user name: <input type=\"text\" id=\"username\" ></input>")
+    .load('/bookhub/login.html')
+    // .html("user name: <input type=\"text\" id=\"username\" ></input>")
     .dialog({
       autoOpen: false,
       modal: true,
