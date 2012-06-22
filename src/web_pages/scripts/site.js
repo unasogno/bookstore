2afc2be5c41c69483a59cd99c15277071a290757
kai cui <unasogno@hotmail.com>
2012-06-22 17:56:30 +0800

    add site.js

--- src/web_pages/scripts/site.js
+++ src/web_pages/scripts/site.js
@@ -1,7 +1,23 @@
 function login(dialog, success, failure) {
-  $(dialog)
-    .load('/bookhub/login.html')
-    .dialog({
+  $(dialog).load('/bookhub/login.html');
+  $(dialog).dialog({
+      height: 200,
+      buttons: {
+        $("#submit_credential").html(): function() {
+        },
+        "清除：": function() {
+          alert($(dialog).html());
+        }
+      },
+      autoOpen: false,
+      modal: true,
+      beforeClose: function (event, ui) {
+        return false;
+      }
+    });
+
+  $(dialog).dialog("open");
+} .dialog({
       height: 200,
       buttons: {
         $("#submit_credential").html(): function() {
